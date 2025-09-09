c2dbe1086eef1bfb328fbea36fe63ab9412dbea3
Luca Scherzer <luca.scherzer@de.clara.net>
2025-09-09 15:23:31 +0200

    build: container image

--- flake.nix
+++ flake.nix
@@ -5,25 +5,54 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { self, flake-utils, naersk, nixpkgs }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      flake-utils,
+      naersk,
+      nixpkgs,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = (import nixpkgs) {
           inherit system;
         };
 
-        naersk' = pkgs.callPackage naersk {};
+        naersk' = pkgs.callPackage naersk { };
 
-      in rec {
+      in
+      rec {
         # For `nix build` & `nix run`:
-        defaultPackage = naersk'.buildPackage {
+        packages.default = naersk'.buildPackage {
           src = ./.;
         };
 
+        # For backward compatibility
+        defaultPackage = packages.default;
+
         # For `nix develop`:
         devShell = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [ rustc cargo ];
+          nativeBuildInputs = with pkgs; [
+            rustc
+            cargo
+          ];
         };
       }
-    );
+    )
+    // {
+      # Dedicated container output that will always build on x86_64-linux
+      packages.x86_64-linux.container =
+        let
+          linuxPkgs = import nixpkgs { system = "x86_64-linux"; };
+          linuxNaersk = linuxPkgs.callPackage naersk { };
+          linuxBinary = linuxNaersk.buildPackage { src = ./.; };
+        in
+        linuxPkgs.dockerTools.buildLayeredImage {
+          name = "eloquentle";
+          config = {
+            Cmd = [ "${linuxBinary}/bin/eloquentle" ];
+          };
+        };
+    };
 }
