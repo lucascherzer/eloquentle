5ece84527ee7c1cda67647b4c7ae6d16dc1d3571
Luca Scherzer <luca.scherzer@de.clara.net>
2025-09-10 09:17:22 +0200

    build: idk something nix related

--- flake.nix
+++ flake.nix
@@ -1,7 +1,11 @@
 {
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    naersk.url = "github:nix-community/naersk";
+    # Explicitly pin naersk to use our nixpkgs
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
@@ -15,24 +19,18 @@
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = (import nixpkgs) {
-          inherit system;
-        };
-
+        pkgs = nixpkgs.legacyPackages.${system};
         naersk' = pkgs.callPackage naersk { };
 
       in
       rec {
         # For `nix build` & `nix run`:
-        defaultPackage = naersk'.buildPackage {
+        packages.default = naersk'.buildPackage {
           src = ./.;
         };
-        packages.container = pkgs.dockerTools.buildLayeredImage {
-          name = "eloquentle";
-          config = {
-            Cmd = [ "${defaultPackage}/bin/eloquentle" ];
-          };
-        };
+
+        # For backward compatibility
+        defaultPackage = packages.default;
 
         # For `nix develop`:
         devShell = pkgs.mkShell {
@@ -42,5 +40,42 @@
           ];
         };
       }
-    );
+    )
+    // {
+      # Dedicated container output that will always build on x86_64-linux
+      packages.x86_64-linux.container =
+        let
+          # Use the same nixpkgs instance
+          linuxPkgs = nixpkgs.legacyPackages.x86_64-linux;
+          linuxNaersk = linuxPkgs.callPackage naersk { };
+
+          # Build the binary for Linux
+          linuxBinary = linuxNaersk.buildPackage {
+            src = ./.;
+            # Optimize for release
+            release = true;
+          };
+        in
+        linuxPkgs.dockerTools.buildLayeredImage {
+          name = "eloquentle";
+          tag = "latest";
+
+          # Only include the binary and necessary runtime files
+          contents = [
+            # Just copy the binary
+            (linuxPkgs.runCommand "eloquentle-binary" { } ''
+              mkdir -p $out/bin
+              cp ${linuxBinary}/bin/eloquentle $out/bin/
+              chmod +x $out/bin/eloquentle
+            '')
+
+            # Include SSL certificates for HTTPS support if needed
+            linuxPkgs.cacert
+          ];
+
+          config = {
+            Cmd = [ "/bin/eloquentle" ];
+          };
+        };
+    };
 }
